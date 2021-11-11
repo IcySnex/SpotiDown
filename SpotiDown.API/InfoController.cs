71fdd4e34d0cefdae4e900747bbe67a8219c30fc
IcySnex <lao43919@gmail.com>
2021-11-11 21:48:27 +0100

    Started Web API to download mp3 on mobile

---
+++ SpotiDown.API/InfoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpotiDown.API
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfoController : ControllerBase
+    {
+        [HttpGet]
+        public string Get() => "{SpotiDown Valid Web API}";
+    }
+}
