1b6e5d2915298a3dca9749e396786961adc73ad7
IcySnex <lao43919@gmail.com>
2022-02-25 19:07:10 +0100

    more work on youtube page

--- SpotiDownWin/SpotiDown/Helpers/Text.cs
+++ SpotiDownWin/SpotiDown/Helpers/Text.cs
@@ -4,7 +4,7 @@ namespace SpotiDown.Helpers;
 
 public class Text
 {
-    public static string FormatTime(TimeSpan Time, string Format = "{hh} hrs, {mm} min, {ss} sec")
+    public static string FormatTime(TimeSpan Time, string Format = "{hh}h, {mm}m, {ss}s")
     {
         if (Time.Hours > 0)
             Format = Format.Replace("{hh}", Time.Hours.ToString());
@@ -12,6 +12,7 @@ public class Text
             Format = Format.Replace("{mm}", Time.Minutes.ToString());
         if (Time.Seconds > 0)
             Format = Format.Replace("{ss}", Time.Seconds.ToString());
+        Format = Format.Replace("{hh}h, ", "").Replace("{mm}m, ", "").Replace("{ss}s", "");
         return Format;
     }
 }
--- SpotiDownWin/SpotiDown/Pages/YouTube.xaml.cs
+++ SpotiDownWin/SpotiDown/Pages/YouTube.xaml.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Animation;
 using SpotiDown.Controls;
 using SpotiDown.Enums;
 using SpotiDown.Models;
 using System;
+using System.Threading.Tasks;
 
 namespace SpotiDown.Pages;
 
@@ -14,7 +17,28 @@ public sealed partial class YouTube : Page
         InitializeComponent();
     }
 
-    private void Search(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    private void UpdateUI(bool WithDownloadBar)
+    {
+        if (WithDownloadBar)
+        {
+            DownloadBar.Visibility = Visibility.Visible;
+            DownloadBar_FadeIn.Begin();
+            ScrollContainer.Margin = new(0, 110, 0, 110);
+        }
+        else
+        {
+            DownloadBar_FadeOut.Begin();
+            ScrollContainer.Margin = new(0, 110, 0, 0);
+        }
+    }
+
+    private void DownloadBar_FadeOut_Completed(object sender, object e) =>
+            DownloadBar.Visibility = Visibility.Collapsed;
+
+    private void Container_SelectionChanged(object sender, SelectionChangedEventArgs e) =>
+        UpdateUI(Container.SelectedItems.Count > 0 ? true : false);
+
+    private void Search_Click(object sender, RoutedEventArgs e)
     {
         SongModel Song = new(
             SongType.YouTube,
@@ -28,6 +52,23 @@ public sealed partial class YouTube : Page
             "https://img.youtube.com/vi/-Mdkbg0nyWA/maxresdefault.jpg",
             1,
             34);
+        SongModel Song1 = new(
+            SongType.YouTube,
+            "https://www.youtube.com/watch?v=-Mdkbg0nyWA&ab_channel=1nonly",
+            "TheseHoes",
+            "SexyMexy",
+            new(0, 2, 21),
+            "HIIII",
+            DateTime.UtcNow,
+            "Hey,\nThis\nis some random ass lyrics\nFUCKKKK\nHelp me\nIM being kidnapped LOOOL\n\n Anyways\nHIIIIII",
+            "https://img.youtube.com/vi/-Mdkbg0nyWA/maxresdefault.jpg",
+            1,
+            34);
         Container.Items.Add(new YoutubeEntry(Song));
+        Container.Items.Add(new YoutubeEntry(Song1));
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
     }
 }
